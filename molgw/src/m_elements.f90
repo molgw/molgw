86f9c0ba018f3f317c4eaeca7f8632a061ce2127
fabien.bruneval <fabien.bruneval@dc766c57-dcc4-b191-729b-831f79607218>
2015-02-01 01:24:08 +0000

    use the symmetry in BSE, TDDFT to halve the A+B, A-B calculation time. Able to read input files with element symbols and element Z numbers

--- molgw/src/m_elements.f90
+++ molgw/src/m_elements.f90
@@ -6,8 +6,13 @@ module m_elements
  use m_mpi
 
 
-
-
+ integer,parameter,private :: nelement_max = 54
+ character(len=2),parameter,private :: element_list(nelement_max) =                            &
+  (/' H',                                                                                'He', &  !  2
+    'Li','Be',                                                  ' B',' C',' N',' O',' F','Ne', &  ! 10
+    'Na','Mg',                                                  'Al','Si',' P',' S','Cl','Ar', &  ! 18
+    ' K','Ca','Sc','Ti',' V','Cr','Mn','Fe','Co','Ni','Cu','Zn','Ga','Ge','As','Se','Br','Kr', &  ! 36
+    'Rb','Sr',' Y','Zr','Nb','Mo','Tc','Ru','Rh','Pd','Ag','Cd','In','Sn','Sb','Te',' I','Xe'  /) ! 54
 
 
 
@@ -29,75 +34,75 @@ function element_covalent_radius(zatom)
  ! They will be converted in bohr just after
  select case(NINT(zatom))
  case( 1)
-   element_covalent_radius =  31
+   element_covalent_radius =  31.
  case( 2)
-   element_covalent_radius =  28
+   element_covalent_radius =  28.
  case( 3)
-   element_covalent_radius = 128
+   element_covalent_radius = 128.
  case( 4)
-   element_covalent_radius =  96
+   element_covalent_radius =  96.
  case( 5)
-   element_covalent_radius =  84
+   element_covalent_radius =  84.
  case( 6)
-   element_covalent_radius =  73
+   element_covalent_radius =  73.
  case( 7)
-   element_covalent_radius =  71
+   element_covalent_radius =  71.
  case( 8)
-   element_covalent_radius =  66
+   element_covalent_radius =  66.
  case( 9)
-   element_covalent_radius =  57
- case(10) ! Ne
-   element_covalent_radius =  58
+   element_covalent_radius =  57.
+ case(10) ! Ne.
+   element_covalent_radius =  58.
  case(11)
-   element_covalent_radius = 166
+   element_covalent_radius = 166.
  case(12)
-   element_covalent_radius = 141
+   element_covalent_radius = 141.
  case(13)
-   element_covalent_radius = 121
+   element_covalent_radius = 121.
  case(14)
-   element_covalent_radius = 111
+   element_covalent_radius = 111.
  case(15)
-   element_covalent_radius = 107
+   element_covalent_radius = 107.
  case(16)
-   element_covalent_radius = 105
+   element_covalent_radius = 105.
  case(17)
-   element_covalent_radius = 102
- case(18) ! Ar
-   element_covalent_radius = 106
+   element_covalent_radius = 102.
+ case(18) ! Ar.
+   element_covalent_radius = 106.
  case(19)
-   element_covalent_radius = 203
+   element_covalent_radius = 203.
  case(20)
-   element_covalent_radius = 176
+   element_covalent_radius = 176.
  case(21)
-   element_covalent_radius = 170
+   element_covalent_radius = 170.
  case(22)
-   element_covalent_radius = 160
+   element_covalent_radius = 160.
  case(23)
-   element_covalent_radius = 153
+   element_covalent_radius = 153.
  case(24)
-   element_covalent_radius = 139
+   element_covalent_radius = 139.
  case(25)
-   element_covalent_radius = 145
+   element_covalent_radius = 145.
  case(26)
-   element_covalent_radius = 145
+   element_covalent_radius = 145.
  case(27)
-   element_covalent_radius = 140
+   element_covalent_radius = 140.
  case(28)
-   element_covalent_radius = 124
+   element_covalent_radius = 124.
  case(29)
-   element_covalent_radius = 132
+   element_covalent_radius = 132.
  case(30)
-   element_covalent_radius = 122
+   element_covalent_radius = 122.
  case(31)
-   element_covalent_radius = 120
+   element_covalent_radius = 120.
  case(32)
-   element_covalent_radius = 119
+   element_covalent_radius = 119.
  case(34)
-   element_covalent_radius = 120
+   element_covalent_radius = 120.
  case(35)
-   element_covalent_radius = 120
- case(36) ! Kr
-   element_covalent_radius = 116
+   element_covalent_radius = 120.
+ case(36) ! Kr.
+   element_covalent_radius = 116.
  case default
    stop'radius not available'
  end select
@@ -110,103 +115,48 @@ end function element_covalent_radius
 
 
 !=========================================================================
+function element_number(element_name)
+ implicit none
+ character(len=2),intent(in) :: element_name
+ integer                     :: element_number
+!=====
+ integer :: ielement
+!=====
+ ielement=1
+ do while( ADJUSTL(element_name) /= ADJUSTL(element_list(ielement)) )
+   if( ielement == nelement_max ) then
+     WRITE_MASTER(*,'(a,a)')    ' Input symbol ',element_name
+     WRITE_MASTER(*,'(a,i3,a)') ' Element symbol is not one of first ',nelement_max,' elements'
+     stop'element symbol not understood'
+   endif
+   ielement = ielement + 1
+ enddo
+
+ element_number = ielement
+ 
+
+end function element_number
+
+
+!=========================================================================
 function element_name(zatom)
  implicit none
  real(dp),intent(in) :: zatom
  character(len=2)  :: element_name
 !=====
+ if( NINT(zatom) == 0 ) then
+   element_name='X'
+   return
+ endif
+ if( NINT(zatom) > nelement_max ) then
+   WRITE_MASTER(*,'(a,i3,a)') 'Element symbol is not one of first ',nelement_max,' elements'
+   stop'element symbol not understood'
+ endif
 
- select case(NINT(zatom))
- case( 0)
-   element_name='X'
- case( 1)
-   element_name='H'
- case( 2)
-   element_name='He'
- case( 3)
-   element_name='Li'
- case( 4)
-   element_name='Be'
- case( 5)
-   element_name='B'
- case( 6)
-   element_name='C'
- case( 7)
-   element_name='N'
- case( 8)
-   element_name='O'
- case( 9)
-   element_name='F'
- case(10)
-   element_name='Ne'
- case(11)
-   element_name='Na'
- case(12)
-   element_name='Mg'
- case(13)
-   element_name='Al'
- case(14)
-   element_name='Si'
- case(15)
-   element_name='P'
- case(16)
-   element_name='S'
- case(17)
-   element_name='Cl'
- case(18)
-   element_name='Ar'
- case(19)
-   element_name='K'
- case(20)
-   element_name='Ca'
- case(21)
-   element_name='Sc'
- case(22)
-   element_name='Ti'
- case(23)
-   element_name='V'
- case(24)
-   element_name='Cr'
- case(25)
-   element_name='Mn'
- case(26)
-   element_name='Fe'
- case(27)
-   element_name='Co'
- case(28)
-   element_name='Ni'
- case(29)
-   element_name='Cu'
- case(30)
-   element_name='Zn'
- case(31)
-   element_name='Ga'
- case(32)
-   element_name='Ge'
- case(33)
-   element_name='As'
- case(34)
-   element_name='Se'
- case(35)
-   element_name='Br'
- case(36)
-   element_name='Kr'
- case(47)
-   element_name='Ag'
- case default
-   WRITE_MASTER(*,*) 'Z too large: element not yet coded'
-   WRITE_MASTER(*,*) 'Give a generic element name: X'
-   element_name='X'
- end select
+ element_name = element_list(NINT(zatom))
+
 
 end function element_name
-
-
-
-
-
-
-
 
 
 
