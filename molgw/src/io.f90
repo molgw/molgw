4965a0c81bff1940f7420bd593f53e10a34ff830
Fabien Bruneval <bruneval@users.noreply.github.com>
2016-06-27 09:31:32 +0200

    Add a preprocessing flag HAVE_FORTRAN2008

--- molgw/src/io.f90
+++ molgw/src/io.f90
@@ -8,7 +8,9 @@
 !=========================================================================
 subroutine header()
  use,intrinsic :: iso_c_binding, only: C_INT
+#ifdef HAVE_FORTRAN2008
  use,intrinsic :: iso_fortran_env, only: compiler_version,compiler_options
+#endif
  use m_definitions
  use m_mpi
  use m_warning,only: issue_warning,msg
@@ -36,6 +38,7 @@ subroutine header()
  write(stdout,'(x,70("="))') 
 
  write(stdout,'(/,a,i6,/)') ' MOLGW revision is',revision
+#ifdef HAVE_FORTRAN2008
  write(stdout,'(x,a,a)')    'compiled with',compiler_version()
  write(stdout,'(x,a)')      'with options: '
  chartmp = compiler_options()
@@ -49,6 +52,7 @@ subroutine header()
    kchar = kchar + lchar
  enddo
  write(stdout,*)
+#endif
 
 
  call date_and_time(VALUES=values)
