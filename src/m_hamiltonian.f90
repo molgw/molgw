3d44a962528e50c7038294063e74c292bca5a2a5
Fabien Bruneval <bruneval@users.noreply.github.com>
2016-12-14 10:20:15 +0100

    BLASify the calculation of the density matrix. Observed speed-up: x 100

--- src/m_hamiltonian.f90
+++ src/m_hamiltonian.f90
@@ -850,13 +850,21 @@ subroutine setup_density_matrix(nbf,nstate,c_matrix,occupation,p_matrix)
  real(dp),intent(out) :: p_matrix(nbf,nbf,nspin)
 !=====
  integer :: ispin,ibf,jbf
+ integer :: istate
 !=====
 
  call start_clock(timing_density_matrix)
+ p_matrix(:,:,:) = 0.0_dp
  do ispin=1,nspin
+   do istate=1,nstate
+     if( occupation(istate,ispin) < completely_empty ) cycle
+     call DSYR('L',nbf,occupation(istate,ispin),c_matrix(:,istate,ispin),1,p_matrix(:,:,ispin),nbf)
+   enddo
+
+   ! Symmetrize
    do jbf=1,nbf
-     do ibf=1,nbf
-       p_matrix(ibf,jbf,ispin) = SUM( occupation(:,ispin) * c_matrix(ibf,:,ispin) * c_matrix(jbf,:,ispin) )
+     do ibf=jbf+1,nbf
+       p_matrix(jbf,ibf,ispin) = p_matrix(ibf,jbf,ispin)
      enddo
    enddo
  enddo
