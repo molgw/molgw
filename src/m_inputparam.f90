6765ad1384bf359cc765889c4be9647f9e6b7637
bruneval <fabien.bruneval@gmail.com>
2017-09-08 09:10:16 +0200

    Allow testing in parallel even without RI. Some tests without RI do not work. Do be fixed in the future.

--- src/m_inputparam.f90
+++ src/m_inputparam.f90
@@ -997,10 +997,10 @@ subroutine read_inputfile_namelist()
  has_auxil_basis = TRIM(auxil_basis_name(1)) /= '' .OR. TRIM(ecp_auxil_basis_name(1)) /= ''
  has_small_basis = TRIM(small_basis_name(1)) /= '' .OR. TRIM(ecp_small_basis_name(1)) /= ''
 
- if( .NOT. has_auxil_basis .AND. nproc_world > 1 ) then
-   write(stdout,*) 'Parallelization is not available without an auxiliary basis'
-   call issue_warning('Please run with one CPU only or provide MOLGW with an auxiliary basis')
- endif
+! if( .NOT. has_auxil_basis .AND. nproc_world > 1 ) then
+!   write(stdout,*) 'Parallelization is not available without an auxiliary basis'
+!   call issue_warning('Please run with one CPU only or provide MOLGW with an auxiliary basis')
+! endif
 
  natom_basis = natom + nghost
 
