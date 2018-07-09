f8cd3c1635245841590fe7f1510b55ac0797ab56
bruneval <fabien.bruneval@gmail.com>
2018-07-09 16:06:00 +0200

    amend previous commit

--- src/libint_onebody_grad.cc
+++ src/libint_onebody_grad.cc
@@ -66,7 +66,7 @@ void libint_overlap_grad(int amA, int contrdepthA , double A [] , double alphaA 
      int12->AB_x[0] = AB_x ;
      int12->AB_y[0] = AB_y ;
      int12->AB_z[0] = AB_z ;
-#if LIBINT2_DEFINED(eri, BA_x) && LIBINT2_DEFINED(eri, BA_y) && LIBINT2_DEFINED(eri, BA_z)
+#ifdef LIBINT2_DEFINED(eri, BA_x) && LIBINT2_DEFINED(eri, BA_y) && LIBINT2_DEFINED(eri, BA_z)
      int12->BA_x[0] = -AB_x ;
      int12->BA_y[0] = -AB_y ;
      int12->BA_z[0] = -AB_z ;
