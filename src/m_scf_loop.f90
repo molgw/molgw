19829e4be5ac3ed825f2e7f1d5d37eae1dbb3bd3
bruneval <fabien.bruneval@gmail.com>
2019-01-21 17:28:04 +0100

    implementation cleaned up. Only scalapack/mpi is missing

--- src/m_scf_loop.f90
+++ src/m_scf_loop.f90
@@ -472,6 +472,13 @@ subroutine scf_loop(is_restart,&
      call selfenergy_set_state_range(nstate,occupation)
      call gw_density_matrix(nstate,basis,occupation,energy,c_matrix,wpol,p_matrix_corr)
      call destroy_spectral_function(wpol)
+   case('GW_IOMEGA')
+     ! This keyword calculates the GW density matrix as it is derived in the new GW theory
+     !calc_type%selfenergy_technique = imaginary_axis
+     call init_spectral_function(nstate,occupation,nomega_imag,wpol)
+     call polarizability_grid_scalapack(basis,nstate,occupation,energy,c_matrix,en%rpa,wpol)
+     call selfenergy_set_state_range(nstate,occupation)
+     call gw_density_matrix_imag(nstate,basis,occupation,energy,c_matrix,wpol,p_matrix_corr)
    end select
 
 
