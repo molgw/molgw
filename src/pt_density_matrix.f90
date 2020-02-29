2851174aae9933b02018129398be9bed67c543fa
Fabien Bruneval <fabien.bruneval@gmail.com>
2020-02-29 17:48:53 +0100

    update the numerical density matrix test values (bug fixed earlier)

--- src/pt_density_matrix.f90
+++ src/pt_density_matrix.f90
@@ -18,7 +18,7 @@ subroutine pt2_density_matrix(nstate,basis,occupation,energy,c_matrix,p_matrix)
   use m_hamiltonian_onebody
   use m_selfenergy_tools
   implicit none
- 
+
   integer,intent(in)         :: nstate
   type(basis_set),intent(in) :: basis
   real(dp),intent(in)        :: occupation(nstate,nspin),energy(nstate,nspin)
@@ -34,73 +34,73 @@ subroutine pt2_density_matrix(nstate,basis,occupation,energy,c_matrix,p_matrix)
   real(dp)                :: num1,num2
   real(dp)                :: p_matrix_pt2(nstate,nstate,nspin)
   !=====
- 
- 
+
+
   call start_clock(timing_mbpt_dm)
- 
+
   write(stdout,'(/,a)') ' Calculate the PT2 density matrix'
- 
+
   if( nspin /= 1 ) call die('pt2_density_matrix: only implemented for spin restricted calculations')
- 
+
   if(has_auxil_basis) then
     call calculate_eri_3center_eigen(c_matrix,ncore_G+1,nvirtual_G-1,ncore_G+1,nvirtual_G-1)
   else
     call calculate_eri_4center_eigen_uks(c_matrix,ncore_G+1,nvirtual_G-1)
   endif
- 
- 
+
+
   ! Full calculation of the PT2 density matrix
- 
+
   p_matrix_pt2(:,:,:) = 0.0_dp
   ! so far, only spin-restricted calculation are possible
   pqspin = 1
- 
+
   ! A1 P_ij sum over k and a,b
   do istate=ncore_G+1,nhomo_G
   do jstate=ncore_G+1,nhomo_G
     do kstate=ncore_G+1,nhomo_G
       do astate=nhomo_G+1,nvirtual_G-1
         do bstate=nhomo_G+1,nvirtual_G-1
- 
+
           denom1 = energy(astate,pqspin) + energy(bstate,pqspin) - energy(istate,pqspin) - energy(kstate,pqspin)
           denom2 = energy(astate,pqspin) + energy(bstate,pqspin) - energy(jstate,pqspin) - energy(kstate,pqspin)
- 
+
           num1 = 2.0_dp * eri_eigen(istate,astate,pqspin,kstate,bstate,pqspin) &
                        - eri_eigen(istate,bstate,pqspin,kstate,astate,pqspin)
           num2 = 2.0_dp * eri_eigen(jstate,astate,pqspin,kstate,bstate,pqspin)
- 
+
           p_matrix_pt2(istate,jstate,pqspin) = p_matrix_pt2(istate,jstate,pqspin)  &
                   - num1 * num2 / ( denom1 * denom2 )
- 
+
         enddo
       enddo
     enddo
   enddo
   enddo
- 
+
   ! A2 P_ab sum over  i,j  and c
   do astate=nhomo_G+1,nvirtual_G-1
   do bstate=nhomo_G+1,nvirtual_G-1
     do cstate=nhomo_G+1,nvirtual_G-1
       do istate=ncore_G+1,nhomo_G
         do jstate=ncore_G+1,nhomo_G
- 
+
           denom1 = energy(istate,pqspin) + energy(jstate,pqspin) - energy(astate,pqspin) - energy(cstate,pqspin)
           denom2 = energy(istate,pqspin) + energy(jstate,pqspin) - energy(bstate,pqspin) - energy(cstate,pqspin)
- 
+
           num1 = 2.0_dp * eri_eigen(astate,istate,pqspin,cstate,jstate,pqspin) &
                        - eri_eigen(astate,jstate,pqspin,cstate,istate,pqspin)
           num2 = 2.0_dp * eri_eigen(bstate,istate,pqspin,cstate,jstate,pqspin)
- 
+
           p_matrix_pt2(astate,bstate,pqspin) = p_matrix_pt2(astate,bstate,pqspin)  &
                   + num1 * num2 / ( denom1 * denom2 )
- 
+
         enddo
       enddo
     enddo
   enddo
   enddo
- 
+
   ! A3    P_cj  sum over i, and a,b
   ! A4    P_jc  sum over i, and a,b
   do cstate=nhomo_G+1,nvirtual_G-1
@@ -112,7 +112,7 @@ subroutine pt2_density_matrix(nstate,basis,occupation,energy,c_matrix,p_matrix)
           denom2 = energy(jstate,pqspin) - energy(cstate,pqspin)
           num1 = 2.0_dp * eri_eigen(jstate,astate,pqspin,istate,bstate,pqspin) - eri_eigen(jstate,bstate,pqspin,istate,astate,pqspin)
           num2 = 2.0_dp * eri_eigen(astate,cstate,pqspin,bstate,istate,pqspin)
- 
+
           p_matrix_pt2(cstate,jstate,pqspin) = p_matrix_pt2(cstate,jstate,pqspin)  &
                                             + num1 * num2 / ( denom1 * denom2 )
           p_matrix_pt2(jstate,cstate,pqspin) = p_matrix_pt2(jstate,cstate,pqspin)  &
@@ -122,7 +122,7 @@ subroutine pt2_density_matrix(nstate,basis,occupation,energy,c_matrix,p_matrix)
     enddo
   enddo
   enddo
- 
+
   ! A5   P_bk  sum over i,j,a
   ! A6   P_kb  sum over i,j,a
   do bstate=nhomo_G+1,nvirtual_G-1
@@ -134,7 +134,7 @@ subroutine pt2_density_matrix(nstate,basis,occupation,energy,c_matrix,p_matrix)
           denom2 = energy(kstate,pqspin) - energy(bstate,pqspin)
           num1 = 2.0_dp * eri_eigen(jstate,astate,pqspin,istate,bstate,pqspin) - eri_eigen(jstate,bstate,pqspin,istate,astate,pqspin)
           num2 = 2.0_dp * eri_eigen(istate,kstate,pqspin,jstate,astate,pqspin)
- 
+
           p_matrix_pt2(bstate,kstate,pqspin) = p_matrix_pt2(bstate,kstate,pqspin)  &
                                             - num1 * num2 / ( denom1 * denom2 )
           p_matrix_pt2(kstate,bstate,pqspin) = p_matrix_pt2(kstate,bstate,pqspin)  &
@@ -144,11 +144,11 @@ subroutine pt2_density_matrix(nstate,basis,occupation,energy,c_matrix,p_matrix)
     enddo
   enddo
   enddo
- 
- 
+
+
   call update_density_matrix(basis%nbf,nstate,occupation,c_matrix,p_matrix_pt2,p_matrix)
- 
- 
+
+
   if( print_density_matrix_ .AND. is_iomaster ) then
     write(stdout,'(1x,a)') 'Write DENSITY_MATRIX file'
     open(newunit=file_density_matrix,file='DENSITY_MATRIX',form='unformatted',action='write')
@@ -157,14 +157,14 @@ subroutine pt2_density_matrix(nstate,basis,occupation,energy,c_matrix,p_matrix)
     enddo
     close(file_density_matrix)
   endif
- 
- 
+
+
   if(has_auxil_basis) then
     call destroy_eri_3center_eigen()
   else
     call destroy_eri_4center_eigen_uks()
   endif
- 
+
   call stop_clock(timing_mbpt_dm)
 
 end subroutine pt2_density_matrix
@@ -652,8 +652,12 @@ subroutine gw_density_matrix_imag(nstate,basis,occupation,energy,c_matrix,wpol,p
 
             v_chi_v_pq = DOT_PRODUCT( eri3_sca_p(:,mlocal) , chi_eri3_sca_q(:,mlocal) )
 
+            ! Factor 2 and the real part come from the integration of Sigma
+            ! for negative AND positive imaginary frequencies
+            ! Sigma_pq(-iw) = Sigma_qp(iw)^*
             p_matrix_gw(pstate,qstate,pqspin) = p_matrix_gw(pstate,qstate,pqspin) &
-                 - SUM( wpol%weight_quad(iomega) * weight_sigma(:) * v_chi_v_pq /  pi**2   &
+                 - spin_fact * 2.0_dp  / (2.0_dp *  pi)**2  &
+                       * SUM( wpol%weight_quad(iomega) * weight_sigma(:) * v_chi_v_pq  &
                     * REAL( (  1.0_dp / ( im * omega_sigma(:) - (energy(mstate,pqspin)-mu) + im * wpol%omega_quad(iomega) )    &
                              + 1.0_dp / ( im * omega_sigma(:) - (energy(mstate,pqspin)-mu) - im * wpol%omega_quad(iomega) )  ) &
                             / ( im * omega_sigma(:) - (energy(pstate,pqspin)-mu) )   &
@@ -767,28 +771,28 @@ subroutine gw_density_matrix_dyson_imag(nstate,basis,occupation,energy,c_matrix,
 
 
   if( has_auxil_basis ) call calculate_eri_3center_eigen(c_matrix,ncore_G+1,nvirtual_G-1,ncore_G+1,nvirtual_G-1)
- 
- 
+
+
   mrange = nvirtual_G - ncore_G - 1
- 
+
   meri3 = NUMROC(nauxil_2center,wpol%desc_chi(MB_),iprow,wpol%desc_chi(RSRC_),nprow)
   neri3 = NUMROC(mrange        ,wpol%desc_chi(NB_),ipcol,wpol%desc_chi(CSRC_),npcol)
   call DESCINIT(desc_eri3_final,nauxil_2center,mrange,wpol%desc_chi(MB_),wpol%desc_chi(NB_), &
                 wpol%desc_chi(RSRC_),wpol%desc_chi(CSRC_),wpol%desc_chi(CTXT_),MAX(1,meri3),info)
- 
+
   call clean_allocate('TMP 3-center MO integrals',eri3_sca_p,meri3,neri3)
   call clean_allocate('TMP 3-center MO integrals',eri3_sca_q,meri3,neri3)
   call clean_allocate('TMP 3-center MO integrals',chi_eri3_sca_q,meri3,neri3)
- 
+
   call DESCINIT(desc_eri3_t,nauxil_2center,mrange,MB_eri3_mo,NB_eri3_mo,first_row,first_col,cntxt_eri3_mo,MAX(1,nauxil_3center),info)
- 
- 
+
+
   !
   ! Set up the imaginary frequency grid
   !
   allocate(omega_sigma(nomega_sigma),weight_sigma(nomega_sigma))
   call coeffs_gausslegint(0.0_dp,1.0_dp,omega_sigma,weight_sigma,nomega_sigma)
- 
+
   write(stdout,'(/,1x,a)') 'Numerical integration on a grid along the imaginary axis'
   ! Variable change [0,1] -> [0,+\inf[
   write(stdout,'(a)') '    #    Frequencies (eV)    Quadrature weights'
@@ -876,10 +880,10 @@ subroutine gw_density_matrix_dyson_imag(nstate,basis,occupation,energy,c_matrix,
       enddo
     enddo
   enddo
- 
+
   call update_density_matrix(basis%nbf,nstate,occupation,c_matrix,p_matrix_gw,p_matrix)
- 
- 
+
+
   if( print_density_matrix_ .AND. is_iomaster ) then
     write(stdout,'(1x,a)') 'Write DENSITY_MATRIX file'
     open(newunit=file_density_matrix,file='DENSITY_MATRIX',form='unformatted',action='write')
@@ -888,14 +892,14 @@ subroutine gw_density_matrix_dyson_imag(nstate,basis,occupation,energy,c_matrix,
     enddo
     close(file_density_matrix)
   endif
- 
- 
+
+
   call clean_deallocate('TMP 3-center MO integrals',eri3_sca_p)
   call clean_deallocate('TMP 3-center MO integrals',eri3_sca_q)
   call clean_deallocate('TMP 3-center MO integrals',chi_eri3_sca_q)
- 
+
   call destroy_eri_3center_eigen()
- 
+
   call stop_clock(timing_mbpt_dm)
 
 end subroutine gw_density_matrix_dyson_imag
