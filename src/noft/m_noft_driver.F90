349d6f08606aab0d3be7cb8fbc17cd8efc75719e
M. Rodriguez-Mayorga <marm3.14@gmail.com>
2022-02-08 16:23:45 +0100

    Error in sw vs sl 1-RDM

--- src/noft/m_noft_driver.F90
+++ src/noft/m_noft_driver.F90
@@ -255,8 +255,8 @@ subroutine run_noft(INOF_in,Ista_in,NBF_tot_in,NBF_occ_in,Nfrozen_in,Npairs_in,&
  enddo
 
  ! Print optimized (spin-with?) 1,2-RDMs
- if(iprintdmn==1) call RDMd%print_swdmn() 
- if(iprintswdmn==1) call RDMd%print_dmn(RDMd%DM2_J,RDMd%DM2_K,RDMd%DM2_L) 
+ if(iprintswdmn==1) call RDMd%print_swdmn() 
+ if(iprintdmn==1) call RDMd%print_dmn(RDMd%DM2_J,RDMd%DM2_K,RDMd%DM2_L) 
 
  ! Print hCORE and ERImol integrals in the last (opt) NO_COEF basis (if lowmemERI=.false. NBF_tot, otherwise only NBF_occ)
  if(iprintints==1) then
