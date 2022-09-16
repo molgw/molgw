c5e4b3f3bef4022fe65b1049676f694db5c87cdd
Fabien Bruneval <fabien.bruneval@cea.fr>
2022-09-16 09:50:32 +0200

    fix indentation in m_warning.f90

--- src/m_warning.f90
+++ src/m_warning.f90
@@ -8,14 +8,14 @@
 !=========================================================================
 #include "molgw.h"
 module m_warning
- use m_definitions
+  use m_definitions
 
 
- integer,parameter          :: NWARNINGMAX=100
- integer,private            :: nwarning
- character(len=128),private :: warning_list(NWARNINGMAX)
+  integer,parameter          :: NWARNINGMAX=100
+  integer,private            :: nwarning
+  character(len=128),private :: warning_list(NWARNINGMAX)
 
- character(len=128)         :: msg
+  character(len=128)         :: msg
 
 
 contains
@@ -23,28 +23,28 @@ contains
 
 !=========================================================================
 subroutine init_warning()
- implicit none
+  implicit none
 
- nwarning=0
- warning_list(:)=''
+  nwarning=0
+  warning_list(:)=''
 
 end subroutine init_warning
 
 
 !=========================================================================
 subroutine issue_warning(msgw)
- implicit none
- character(len=*),intent(in) :: msgw
-!=====
+  implicit none
+  character(len=*),intent(in) :: msgw
+  !=====
 
- write(stdout,'(/,a,a)') ' WARNING: ',TRIM(msgw)
+  write(stdout,'(/,a,a)') ' WARNING: ',TRIM(msgw)
 
- !
- ! Eliminate the storage of identical warnings
- if(ANY(warning_list(1:nwarning)==TRIM(msgw))) return
+  !
+  ! Eliminate the storage of identical warnings
+  if(ANY(warning_list(1:nwarning)==TRIM(msgw))) return
 
- nwarning = nwarning + 1
- warning_list(nwarning) = TRIM(msgw)
+  nwarning = nwarning + 1
+  warning_list(nwarning) = TRIM(msgw)
 
 end subroutine issue_warning
 
@@ -52,48 +52,49 @@ end subroutine issue_warning
 !=========================================================================
 subroutine output_all_warnings()
  implicit none
-!=====
- integer :: iwarning
-!=====
 
- if(nwarning>0) then
-   write(stdout,'(/,a,/)') ' SUMMARY of all the WARNINGS issued during the run:'
-   do iwarning=1,nwarning
-     write(stdout,'(i2,a,5x,a)') iwarning,'.',warning_list(iwarning)
-   enddo
-   write(stdout,'(/)')
- endif
+  !=====
+  integer :: iwarning
+  !=====
+
+  if(nwarning>0) then
+    write(stdout,'(/,a,/)') ' SUMMARY of all the WARNINGS issued during the run:'
+    do iwarning=1,nwarning
+      write(stdout,'(i2,a,5x,a)') iwarning,'.',warning_list(iwarning)
+    enddo
+    write(stdout,'(/)')
+  endif
 
 end subroutine output_all_warnings
 
 
 !=========================================================================
 subroutine die(msg)
- implicit none
- character(*),intent(in) :: msg
+  implicit none
+  character(*),intent(in) :: msg
 !=====
 !=====
 
- write(stdout,'(/,a)') '=============================='
- write(stdout,'(a,a)') 'STOP: ',msg
- write(stdout,'(a,/)') '=============================='
- write(stderr,'(a,a)') 'STOP: ',msg
- error stop
+  write(stdout,'(/,a)') '=============================='
+  write(stdout,'(a,a)') 'STOP: ',msg
+  write(stdout,'(a,/)') '=============================='
+  write(stderr,'(a,a)') 'STOP: ',msg
+  error stop
 
 end subroutine die
 
 
 !=========================================================================
 subroutine assert_experimental()
- implicit none
+  implicit none
 !=====
 !=====
 
 #if defined(ACTIVATE_EXPERIMENTAL)
- call issue_warning('Activating an experimental part of the code. Hopefully you know what you are doing')
+  call issue_warning('Activating an experimental part of the code. Hopefully you know what you are doing')
 #else
- write(stdout,*) 'This part of the code is experimental. It should not be used unless you know what you are doing'
- call die('Compilation with -DACTIVATE_EXPERIMENTAL is required')
+  write(stdout,*) 'This part of the code is experimental. It should not be used unless you know what you are doing'
+  call die('Compilation with -DACTIVATE_EXPERIMENTAL is required')
 #endif
 
 end subroutine assert_experimental
