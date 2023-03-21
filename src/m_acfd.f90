b96112ecd3a7d96d925b6de8ae225cf80adb1176
MAU ALICANTE <marm3.14@gmail.com>
2023-03-21 16:59:10 +0100

    Print scaled RPAx and RPA for DHs

--- src/m_acfd.f90
+++ src/m_acfd.f90
@@ -43,27 +43,52 @@ subroutine acfd_total_energy(basis,nstate,occupation,energy,c_matrix,en_mbpt)
   call destroy_spectral_function(wpol)
   if( TRIM(postscf) == 'RPA' ) then
     en_mbpt%rpa = erpa_singlet
+
+    if(kappa_hybrid/=zero) then ! Double-hybrids using RPA (and RPA versions)
+      write(stdout,'(/,a,f16.10)') ' RPA Energy scaled by :',kappa_hybrid
+      en_mbpt%rpa=kappa_hybrid*en_mbpt%rpa
+    endif
     write(stdout,'(a,2x,f19.10)') ' RPA Energy      (Ha):',en_mbpt%rpa
 
-    en_mbpt%total = en_mbpt%nuc_nuc + en_mbpt%kinetic + en_mbpt%nucleus + en_mbpt%hartree + en_mbpt%exx + en_mbpt%rpa
+    if(kappa_hybrid/=zero) then ! Double-hybrids using RPA (and RPA versions)
+      en_mbpt%total = en_mbpt%total + en_mbpt%rpa
+    else
+      en_mbpt%total = en_mbpt%nuc_nuc + en_mbpt%kinetic + en_mbpt%nucleus + en_mbpt%hartree + en_mbpt%exx + en_mbpt%rpa
+    endif
+
     write(stdout,*)
     write(stdout,'(a,2x,f19.10)') ' RPA Total Energy (Ha):',en_mbpt%total
     write(stdout,*)
+
   else
     en_mbpt%rpa = 0.50_dp * erpa_singlet
+    if(kappa_hybrid/=zero) then ! Double-hybrids using RPA (and RPA versions)
+      write(stdout,'(/,a,f16.10)') ' Singlet RPAx Energy scaled by :',kappa_hybrid
+      en_mbpt%rpa=kappa_hybrid*en_mbpt%rpa
+    endif
     write(stdout,'(a,2x,f19.10)') ' Singlet RPAx Energy contribution      (Ha):',en_mbpt%rpa
 
     call init_spectral_function(nstate,occupation,0,wpol)
     call polarizability(.FALSE.,.FALSE.,basis,occupation,energy,c_matrix,erpa_triplet,egw_tmp,wpol,enforce_spin_multiplicity=3)
     call destroy_spectral_function(wpol)
+    if(kappa_hybrid/=zero) then ! Double-hybrids using RPA (and RPA versions)
+      write(stdout,'(/,a,f16.10)') ' Triplet RPAx Energy scaled by :',kappa_hybrid
+      erpa_triplet=kappa_hybrid*erpa_triplet
+    endif
     write(stdout,'(a,2x,f19.10)') ' Triplet RPAx Energy contribution      (Ha):',1.50_dp * erpa_triplet
     en_mbpt%rpa = en_mbpt%rpa + 1.50_dp * erpa_triplet
     write(stdout,'(a,2x,f19.10)') ' RPAx Energy      (Ha):',en_mbpt%rpa
 
-    en_mbpt%total = en_mbpt%nuc_nuc + en_mbpt%kinetic + en_mbpt%nucleus + en_mbpt%hartree + en_mbpt%exx + en_mbpt%rpa
+    if(kappa_hybrid/=zero) then ! Double-hybrids using RPA (and RPA versions)
+      en_mbpt%total = en_mbpt%total + en_mbpt%rpa
+    else
+      en_mbpt%total = en_mbpt%nuc_nuc + en_mbpt%kinetic + en_mbpt%nucleus + en_mbpt%hartree + en_mbpt%exx + en_mbpt%rpa
+    endif
+
     write(stdout,*)
     write(stdout,'(a,2x,f19.10)') ' RPAx Total Energy (Ha):',en_mbpt%total
     write(stdout,*)
+
   endif
   call print_energy_yaml('mbpt energy',en_mbpt)
 
