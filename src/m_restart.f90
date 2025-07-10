9bc0187e4a94b8105686cc791c165c8ec0b16db2
Fabien Bruneval <fabien.bruneval@.cea.fr>
2025-07-10 10:43:28 +0200

    bug fix in a fortran format

--- src/m_restart.f90
+++ src/m_restart.f90
@@ -289,7 +289,7 @@ subroutine read_restart(restart_type, restart_filename, basis, &
     if( temperature > 1.0e-8_dp) then
       occupation(1:nstate_safe, 1) = occupation_read(1:nstate_safe, 1)
       occupation(1:nstate_safe, nspin) = occupation_read(1:nstate_safe, nspin_read)
-      write(stdout,'(1xa)') "Reading occupations from a RESTART file"
+      write(stdout, '(1x,a)') "Reading occupations from a RESTART file"
       call dump_out_occupation('=== Occupations ===', occupation)
     else
       call issue_warning('RESTART file: Occupations are different. Keep those from the current MOLGW run.')
